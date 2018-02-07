92a5b473253f221827f98c6374a24b135a5ba5e6
Maria Virginia <mariav53@gmail.com>
2018-02-07 13:58:51 +0100

    basic web structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '././scss/main.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+				<header>
+					<nav></nav>
+					<button className="button--coupon">cupón</button>
+					<img />
+				</header>
+				<main>
+					<section className="cover"></section>
+					<section className="info"></section>
+					<section className="indicators"></section>
+					<section className="demo">
+						<button className="button--coupon">cupón</button>
+						<button className="button--report"></button>
+					</section>
+					<section className="faqs"></section>
+				</main>
+				<footer>
+					<img />
+				</footer>
       </div>
     );
   }
