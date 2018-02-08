fe3e2987ac36c4b28053075ce524c2b425e6d5fb
gloriafercu <gloriafercu@gmail.com>
2018-02-08 13:55:26 +0100

    add popup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+
+import Contact from './components/Contact';
 import Cover from './components/Cover';
+import Demo from './components/Demo';
+import Faq from './components/Faq';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Indicators from './components/Indicators';
-import Contact from './components/Contact';
 import Info from './components/Info';
+import Popup from './components/Popup';
 
 
 
@@ -14,6 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+				<Popup />
 				<Header />
 				<main>
 					<Cover />
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,17 +4,7 @@ class Contact extends React.Component {
 	render(){
 		return(
 			<section className="contact">
-				<p className="contact__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-				<form className="contact__form">
-					<div className="name-email">
-						<input className="form__name" type="text" placeholder="Nombre" required="required"></input>
-						<input className="form__email" type="email" placeholder="Email" required="required"></input>
-					</div>
-					<div className="message-button">
-						<textarea col="50" row="70" className="form__message" type="text" placeholder="Mensaje" required="required"></textarea>
-						<button className="form__button">Enviar</button>
-					</div>
-				</form>
+				<button className="button--contact">Enviar</button>
 			</section>
 		)
 	}
