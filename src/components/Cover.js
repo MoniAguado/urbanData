aa4efa6fe7dc0c3296b4091567b060256e155b21
Adalaber <maguado85@gmail.com>
2018-02-09 08:39:44 +0100

    cover r photo

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import madridCover from '../images/madridCover.jpg';
+import nyCover from '../images/ny2.jpg';
 
 class Cover extends React.Component{
 	render(){
 		return(
 			<section className="cover">
-				<div className="background" style={{backgroundImage:`url(${madridCover})`}}>
-					<img src={madridCover} className="cover__photo" alt="cover"></img>
+				<div className="background" style={{backgroundImage:`url(${nyCover})`}}>
+					<img src={nyCover} className="cover__photo" alt="cover"></img>
 					<div className="title-block">
 						<h2 className="cover__title">Introduce inteligencia inmobiliaria en tu negocio via APIs</h2>
 						<button className="cover__button">Más info</button>
