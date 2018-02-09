6786b26d4dea9ddfc055803c41d63a9a98708634
Maria Virginia <mariav53@gmail.com>
2018-02-09 08:51:15 +0100

    header logos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logouda from './../images/udaWhite.png';
+import logoapi from './../images/REDapi.png';
 
 class Header extends Component {
   render() {
@@ -7,7 +8,7 @@ class Header extends Component {
     	<header className="header">
 				<div className="header__container">
 					<div className="header__item--big">
-						<img src="http://placehold.it/70/70" alt="Urban Data Analytics logo" className="header__image" />
+						<img src={logoapi} alt="RED api logo" className="header__image" />
 					</div>
 					<div className="header__item">
 						<nav className="header__nav">
