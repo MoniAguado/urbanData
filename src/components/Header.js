14134e454b6e562a5d1dba5174a5c206c1f7007a
Maria Virginia <mariav53@gmail.com>
2018-02-10 20:26:58 +0100

    responsive menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,29 @@ import logouda from './../images/udaWhite.png';
 import logoapi from './../images/REDapi.png';
 
 class Header extends Component {
+
+	burgerToggle() {
+	let linksEl = document.querySelector('.nav');
+	if (linksEl.style.display === 'block') {
+		linksEl.style.display = 'none';
+	} else {
+		linksEl.style.display = 'block';
+	}
+}
   render() {
     return (
     	<header className="header">
 				<div className="header__container">
+					<nav className="header__nav__mobile">
+						<i className="fa fa-bars fa-2x" onClick={this.burgerToggle}></i>
+						<ul className="nav">
+							<li className="nav__item" onClick={this.burgerToggle}><a href="#">¿Qué es?</a></li>
+							<li className="nav__item" onClick={this.burgerToggle}><a href="#">¿Cómo funciona?</a></li>
+							<li className="nav__item" onClick={this.burgerToggle}><a href="#">Demo</a></li>
+							<li className="nav__item" onClick={this.burgerToggle}><a href="#">Faq</a></li>
+							<li className="nav__item" onClick={this.burgerToggle}><a href="#">Contacto</a></li>
+						</ul>
+					</nav>
 					<div className="header__item--big">
 						<img src={logoapi} alt="RED api logo" className="header__image" />
 					</div>
@@ -24,6 +43,7 @@ class Header extends Component {
 					<div className="header__item">
 						<img src={logouda} alt="Urban Data Analytics logo" className="header__image--small" />
 					</div>
+
 				</div>
 			</header>
     );
