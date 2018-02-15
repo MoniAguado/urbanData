3034a7708452806f64b23febbbd156174477daf2
Adalaber <maguado85@gmail.com>
2018-02-15 09:24:14 +0100

    ncover and awards

---
+++ src/components/Awards.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Awards extends React.Component {
+	render(){
+		return(
+
+		)
+	}
+}
+
+export default Awards;
--- src/components/Cover.js
+++ src/components/Cover.js
@@ -10,6 +10,7 @@ class Cover extends React.Component{
 					<img src={nyCover} className="cover__photo" alt="cover"></img>
 					<div className="title-block">
 						<h1 className="cover__title">Introduce inteligencia inmobiliaria en tu negocio via APIs</h1>
+						<h2 className="cover__subtitle">Accede a toda la información clave del mercado inmobiliario de manera instantánea.</h2>
 					</div>
 				</div>
 			</section>
