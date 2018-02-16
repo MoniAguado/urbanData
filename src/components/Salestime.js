734d83d2eae0ac81b1c5114a40ee9cbe1884d079
Teresa Marban <teresamarbancastro@gmail.com>
2018-02-16 10:44:26 +0100

    salestime

--- src/components/Salestime.js
+++ src/components/Salestime.js
@@ -20,7 +20,35 @@ class Salestime extends Component {
 
 	render() {
 		return (
-			<div className="testfetch">
+			<div className="salestime__container">
+				<div className="salestime__description">
+					<h5 className="salestime__title">Tiempo de venta</h5><span>(sem)</span>
+					<p className="salestime__text">Semanas estimadas para la venta de un inmueble en un trimestre dado.</p>
+				</div>
+				<div className="salestime__all-data">
+					<div className="salestime__data">
+						<h6 className="salestime__data--title">Tipología/Barrio</h6>
+						<div className="salestime__data--numbers">
+							<p className="salestime__data--number">13,17 sem</p>
+							<p className="salestime__data--number">4 sem <span>*</span></p>
+						</div>
+					</div>
+					<div className="salestime__data">
+						<h6 className="salestime__data--title">Barrio</h6>
+						<div className="salestime__data--numbers">
+							<p className="salestime__data--number">13,17 sem</p>
+							<p className="salestime__data--number">4 sem <span>*</span></p>
+						</div>
+					</div>
+					<div className="salestime__data">
+						<h6 className="salestime__data--title">Ciudad</h6>
+						<div className="salestime__data--numbers">
+							<p className="salestime__data--number">13,17 sem</p>
+							<p className="salestime__data--number">4 sem <span>*</span></p>
+						</div>
+					</div>
+				</div>
+
 
 			</div>
 		);
