14425e0a542ed3ea1dc0926129b16e387c7e09be
Teresa Marban <teresamarbancastro@gmail.com>
2018-02-16 13:53:33 +0100

    salestime API

--- src/components/Salestime.js
+++ src/components/Salestime.js
@@ -6,9 +6,11 @@ class Salestime extends Component {
 	constructor(props){
 		super(props);
 		this.state={
-			salesTime: ''
+			neighbourhood: '',
+			district: ''
 		}
 	}
+
 
 	componentDidMount(){
 		let url = 'http://reds.urbandataanalytics.com/urban/api/v1.0/indicators?keys=s_t&operations=1&geo_json={"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[-3.6938510999999608,40.4291744]},"properties":{"admin_levels":[3,4]}}]}&period_codes=2017Q3';
@@ -19,45 +21,47 @@ class Salestime extends Component {
 	fetch(url, {method:'GET',
 		headers: headers
 	})
-	.then(response => response.json())
-	.then(json => console.log(json));
+		.then(response => response.json())
+		.then(json => {
+			const districtC =  json["2017Q3"]["72400013000280007900000000000000000000"]["1"]["s_t"];
+			const neighbourhoodC =
+			json["2017Q3"]["72400013000280007900007000000000000000"]["1"]["s_t"];
+			this.setState({
+				neighbourhood: neighbourhoodC,
+				district: districtC
+			})
+			console.log(json);
+			})
+		}
 
-const salestime =  json["2017Q3"]["72400013000280007900007000740007600000"]["renthog_06_13_M"]["0"];
 
-	this.setState({
-		salesTime: salestime
-	})
-
-}
 
 	render() {
 		return (
 			<div className="salestime__container">
 				<div className="salestime__description">
-					<h5 className="salestime__title">Tiempo de venta</h5><span>(sem)</span>
-					<span className="income_value">{this.state.salesTime.toLocaleString()}  €/año</span>
+					<h5 className="salestime__title">Tiempo de venta</h5>
 					<p className="salestime__text">Semanas estimadas para la venta de un inmueble en un trimestre dado.</p>
 				</div>
 				<div className="salestime__all-data">
 					<div className="salestime__data">
 						<h6 className="salestime__data--title">Tipología/Barrio</h6>
 						<div className="salestime__data--numbers">
-							<p className="salestime__data--number">13,17 sem</p>
-							<p className="salestime__data--number">4 sem <span>*</span></p>
+							{/* <p className="salestime__data--number">4 sem <span>*</span></p> */}
 						</div>
 					</div>
 					<div className="salestime__data">
 						<h6 className="salestime__data--title">Barrio</h6>
 						<div className="salestime__data--numbers">
-							<p className="salestime__data--number">13,17 sem</p>
-							<p className="salestime__data--number">4 sem <span>*</span></p>
+							<p className="salestime__data--number">	<span className="salestime_value">{this.state.neighbourhood.toLocaleString()}  sem </span></p>
+							{/* <p className="salestime__data--number">4 sem <span>*</span></p> */}
 						</div>
 					</div>
 					<div className="salestime__data">
 						<h6 className="salestime__data--title">Ciudad</h6>
 						<div className="salestime__data--numbers">
-							<p className="salestime__data--number">13,17 sem</p>
-							<p className="salestime__data--number">4 sem <span>*</span></p>
+							<p className="salestime__data--number">	<span className="salestime_value">{this.state.district.toLocaleString()}  sem </span></p>
+							{/* <p className="salestime__data--number">4 sem <span>*</span></p> */}
 						</div>
 					</div>
 				</div>
