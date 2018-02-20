e136f7f4d46e20b8159639f6c2363158c6dd47b5
Adalaber <maguado85@gmail.com>
2018-02-20 08:29:45 +0100

    trying google maps

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -6,6 +6,38 @@ import Salestime from './Salestime';
 // import Services from './Services';
 
 class Demo extends Component{
+	// constructor(props){
+	// 	super(props);
+	// 	this.state = {
+	// 		value = '',
+	// 		disable = false
+	// 	}
+	//
+	// 	fillInAddress(){
+	// 		const place = autocomplete.getPlace();
+	// 	}
+	//
+	//
+	// 	function fillInAddress() {
+  // // Get the place details from the autocomplete object.
+  // var place = autocomplete.getPlace();
+	//
+  // for (var component in componentForm) {
+  //   document.getElementById(component).value = '';
+  //   document.getElementById(component).disabled = false;
+  // }
+	//
+  // // Get each component of the address from the place details
+  // // and fill the corresponding field on the form.
+  // for (var i = 0; i < place.address_components.length; i++) {
+  //   var addressType = place.address_components[i].types[0];
+  //   if (componentForm[addressType]) {
+  //     var val = place.address_components[i][componentForm[addressType]];
+  //     document.getElementById(addressType).value = val;
+  //   }
+  // }
+}
+	}
 
 	render(){
 		return(
