297df43b813e53eee850b258b441520aa14f82d2
gloriafercu <gloriafercu@gmail.com>
2018-02-23 22:57:11 +0100

    add modal iframe

--- src/components/Carto.js
+++ src/components/Carto.js
@@ -16,28 +16,22 @@ class Carto extends Component {
 	}
 
 	render() {
-		var isOpen = {
-				display: this.state.isOpen ? "none" : "block"
-			};
-
-			var isClose = {
-				display: this.state.isOpen ? "block" : "none"
-			}
 
 		return (
 			<div className="carto__container">
-				{/* <div id="map"></div> */}
-				{/* <div className="carto"> */}
 				<iframe width="100%" height="100%" frameBorder="0" src="https://uda-maps.carto.com/u/udasaas/builder/608abb17-ee1f-4550-a7a4-490e5fe6f4e6/embed" allowFullScreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
 				<div className="share_map">
 					<button type="button" className="button--share" onClick={this.showIframe}><i class="fas fa-share-alt"></i> COMPARTIR</button>
 				</div>
-				<div class="popup_iframe" style={ isClose }>
-					<h3 className="graphic__title">Incorporar mapa</h3>
-					<div className="code__iframe">
-						<p>aqui va el codigo del iframe que no se como pegar .... a lo mejro capando las ("")</p>
+				<div className={this.state.isOpen? "iframe--visible" : "iframe"}>
+					<div className="popup_iframe">
+						<h3 className="graphic__title">Incorporar mapa</h3>
+						<div className="code__iframe">
+							<p><textarea className="textarea__iframe" value="&lt;iframe width=&quot;100%&quot; height=&quot;100%&quot; src=&quot;https://uda-maps.carto.com/u/udasaas/builder/608abb17-ee1f-4550-a7a4-490e5fe6f4e6/embed&quot; frameborder=&quot;0&quot; allowfullscreen&gt;&lt;/iframe&gt;"></textarea></p>
 						</div>
+						<button onClick={this.showIframe} type="button" className="button button--close">X</button>
 					</div>
+				</div>
 			</div>
 		);
 	}
